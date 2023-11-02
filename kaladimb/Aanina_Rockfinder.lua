d9dcc6bc0ec39a6ec57fa4bf0be838000bbb5ce0
Thomas Silloway <tcsillow@gmail.com>
2023-11-01 23:08:27 -0400

    Add enchanter reference to kaladim merchant

--- kaladimb/Aanina_Rockfinder.lua
+++ kaladimb/Aanina_Rockfinder.lua
@@ -1,3 +1,11 @@
+function event_say(e)
+	if(e.other:IsSelfFound() == 1 or e.other:IsSoloOnly() == 1) then
+		if(e.message:findi("Hail")) then
+			e.self:Say("Kinool Goldsinger awaits your arrival in the mystical confines of the Keepers of the Art in Felwithe. A master enchanter, Kinool has the power to weave spells that bind the essence of magic to your silver and gold. Go forth and ask him about enchantments.");
+		end
+	end
+end
+
 function event_trade(e)
 	local item_lib = require("items");
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 20879})) then
