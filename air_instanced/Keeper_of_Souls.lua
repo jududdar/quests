22ba04945d2b49dfb3867e3e392043267259b7f0
Lead Generalist MTF Engineer <espause0703@gmail.com>
2024-01-01 12:29:09 -0500

    Instances, too

--- air_instanced/Keeper_of_Souls.lua
+++ air_instanced/Keeper_of_Souls.lua
@@ -1,24 +1,5 @@
 -- Epic NPC -- Keeper_of_Souls
 
-function event_spawn(e)
-	eq.set_timer("summit",2880000);
-end
-
-function event_timer(e)
-	if(e.timer == "summit") then
-		eq.stop_timer("summit");
-		eq.depop();
-	end
-end
-
-function event_combat(e)
-	if(e.joined) then
-		eq.stop_timer("summit");
-	else
-		eq.set_timer("summit",2880000);
-	end
-end
-
 function event_death_complete(e)
 	local sirranName = "sirran";
 	sirranName = sirranName .. eq.get_zone_guild_id();
