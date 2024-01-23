07e94aba13a41b5cfdf454764791947dd5e0847e
Lead Generalist MTF Engineer <espause0703@gmail.com>
2024-01-22 20:27:03 -0500

    Sister is now farmable in OW and instances.

--- airplane/#Sister_of_the_Spire.lua
+++ airplane/#Sister_of_the_Spire.lua
@@ -3,7 +3,7 @@ function event_death_complete(e)
 	sirranName = sirranName .. eq.get_zone_guild_id();
 
 	eq.set_global(sirranName,"7",3,"M20");
-	eq.spawn2(71058,0,0,-960,-1037,1093,64); -- NPC: Sirran_the_Lunatic
+	eq.unique_spawn(71058,0,0,-960,-1037,1093,64); -- NPC: Sirran_the_Lunatic
 end
 
 -------------------------------------------------------------------------------------------------
